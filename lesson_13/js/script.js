55e86c19d636a8c4d52d1d53ba9de7d4da4fb333
dum4uk <89747200+dum4uk@users.noreply.github.com>
2025-01-16 10:54:51 +0200

    Add files via upload

---
+++ lesson_13/js/script.js
@@ -0,0 +1,28 @@
+"use strick"
+let varOne = "1"
+let varTwo = 3
+let varThree = 3
+let varFour = 4
+let test
+
+if (test= varTwo === 3 || varOne < 3) {
+    console.log("Правда")
+} else {
+    console.log("Ложь");
+}
+
+if (varThree<4 || varFour<6) {
+    console.log("Правда")
+} else {
+    console.log("Ложь");
+}
+
+if (true||false && true||true) {
+    console.log("Правда")
+} else {
+    console.log("Ложь");
+}
+
+if (test=false||true||false) {
+    console.log(test)
+}
